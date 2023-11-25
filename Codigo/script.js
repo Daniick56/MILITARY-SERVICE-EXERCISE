51e676fdbaff4fae06f9a07ca2be0cd85db8fd2b
Daniick56 <ballesterosdeveloper56@gmail.com>
2023-11-25 09:56:17 -0500

    Changing the languaje to english

--- Codigo/script.js
+++ Codigo/script.js
@@ -1,46 +1,43 @@
 
 
-function determinarApto(genero, estatura, edad, icfes) {
+function determineEligibility(gender, height, age, icfes) {
 
-    let matricula = 11800000;
+    let tuition = 11800000;
 
     if (
-        (genero == "m" && estatura >= 1.70 && (edad >= 18 && edad <= 25) && icfes >= 60) ||
-        (genero == "f" && estatura >= 1.50 && (edad >= 18 && edad <= 28) && icfes >= 60)
+        (gender === "m" && height >= 1.70 && (age >= 18 && age <= 25) && icfes >= 60) ||
+        (gender === "f" && height >= 1.50 && (age >= 18 && age <= 28) && icfes >= 60)
     ) {
         
         if (icfes < 65) {
-            let precio1 = matricula * 1.25;
-            console.log(`Usted aplica y el valor de la matricula es de $${precio1} pesos.`);
+            let price1 = tuition * 1.25;
+            console.log(`You are eligible, and the tuition fee is $${price1} pesos.`);
             return; 
-        }else if ((icfes > 66 && icfes < 70) && edad > 22) {
-            console.log(`Usted aplica y el valor de la matricula es de $${matricula} pesos.`);
+        } else if ((icfes > 66 && icfes < 70) && age > 22) {
+            console.log(`You are eligible, and the tuition fee is $${tuition} pesos.`);
             return; 
-        }else if ((icfes > 66 && icfes < 70) && edad <= 22) {
-            let precio3 = matricula * 0.75;
-            console.log(`Usted aplica y el valor de la matricula es de $${precio3} pesos.`);
+        } else if ((icfes > 66 && icfes < 70) && age <= 22) {
+            let price3 = tuition * 0.75;
+            console.log(`You are eligible, and the tuition fee is $${price3} pesos.`);
             return; 
-        }else if (icfes > 70) {
-            let precio4 = matricula * 0.5;
-            console.log(`Usted aplica y el valor de la matricula es de $${precio4} pesos.`);
+        } else if (icfes > 70) {
+            let price4 = tuition * 0.5;
+            console.log(`You are eligible, and the tuition fee is $${price4} pesos.`);
             return; 
-        }else {
-            console.log(`Usted aplica y el valor de la matricula es de $${matricula} pesos.`);
+        } else {
+            console.log(`You are eligible, and the tuition fee is $${tuition} pesos.`);
             return;
         }
-    }else {
-        console.log("Usted no aplica");
-        location.reload;
+    } else {
+        console.log("You are not eligible");
+        location.reload();
         return;
     }
 }
 
-let genero = prompt("Ingresa tu genero (F/M)").toLowerCase();
-let edad = parseInt(prompt(" Ingrese su edad"));
-let icfes = parseInt(prompt(" Ingrese su puntaje de el icfes"));
-let estatura = parseFloat(prompt(" Ingrese su estatura "));  
+let gender = prompt("Enter your gender (F/M)").toLowerCase();
+let age = parseInt(prompt("Enter your age"));
+let icfes = parseInt(prompt("Enter your ICFES score"));
+let height = parseFloat(prompt("Enter your height"));  
 
-
-
-determinarApto(genero, estatura, edad, icfes);
-
+determineEligibility(gender, height, age, icfes);
